e398d102d6a9353ebe4e0c824621a0dc6b1b539e
7zZFHpYyu9 <jianguoh003@gmail.com>
2024-04-08 14:49:49 +0800

    2024-04-08

--- CompoundInterest.java
+++ CompoundInterest.java
@@ -2,7 +2,6 @@
  * This program shows how to store tabular data in a 2D array
  */
 
-
 public class CompoundInterest {
     public static void main(String[] args) {
         final double STARTRATE = 10;
@@ -21,9 +20,8 @@ public class CompoundInterest {
             balances[0][j] = 10000; // Initial balance for the first year
         }
 
-        for (int i = 0; i < balances.length; i++)
-            for (int j = 0; j < balances[j].length; j++)
-            {
+        for (int i = 1; i < balances.length; i++) {
+            for (int j = 0; j < balances[i].length; j++) {
                 // get last year's balances from previous row
                 double oldBalance = balances[i-1][j];
 
@@ -33,21 +31,21 @@ public class CompoundInterest {
                 // compute this year's balances
                 balances[i][j] = oldBalance + interest;
             }
+        }
 
-            // print one row of interest rates
-            for (int j = 0; j < interestRate.length; j++)
-                System.out.printf("%9.0f%%", 100*interestRate[j]);
+        // print one row of interest rates
+        for (int j = 0; j < interestRate.length; j++)
+            System.out.printf("%9.0f%%", 100*interestRate[j]);
+
+        System.out.println();
+
+        // print balance table
+        for (double[] row : balances) {
+            // print balance table
+            for (double b : row)
+                System.out.printf("%10.2f", b);
 
             System.out.println();
-
-            // print balance table
-            for (double[] row : balances)
-            {
-                // print balance table
-                for (double b : row)
-                    System.out.printf("%10.2f", b);
-
-                System.out.println();
-            }
+        }
     }
 }
