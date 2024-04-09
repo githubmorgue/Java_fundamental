1c84d6e3bca0b1aa09b7e23ec207ee729ec9520f
7zZFHpYyu9 <jianguoh003@gmail.com>
2024-04-09 10:04:24 +0800

    static method

--- StaticTest.java
+++ StaticTest.java
@@ -5,35 +5,35 @@
 public class StaticTest {
     public static void main(String[] args) {
         // fill the staff array with three Employee objects
-        var staff = new Employee[3];
+        var staff = new Employee1[3];
 
-        staff[0] = new Employee("Tom", 40000);
-        staff[1] = new Employee("Dick", 60000);
-        staff[2] = new Employee("Harry", 65000);
+        staff[0] = new Employee1("Tom", 40000);
+        staff[1] = new Employee1("Dick", 60000);
+        staff[2] = new Employee1("Harry", 65000);
 
         // print out information about all Employee objects
-        for (Employee e : staff) {
+        for (Employee1 e : staff) {
             System.out.println("name" + e.getName() + ",id=" + e.getId() + ",salary="
                  + e.getSalary());
         }
 
-        int n = Employee.getNextId(); // calls static method
+        int n = Employee1.getNextId(); // calls static method
         System.out.println("Next available id=" + n);
     }
 }
 
 /**
- * Employee
+ * Employee1
  */
 
-class Employee {
+class Employee1 {
     private static int nextId= 1;
 
     private String name;
     private double salary;
     private int id;
 
-    public Employee(String n, double s) {
+    public Employee1(String n, double s) {
         name = n;
         salary = s;
         id = advanceId();
@@ -63,7 +63,7 @@ class Employee {
 
     // unit test
     public static void main(String[] args) {
-        var e = new Employee("Harry", 50000);
+        var e = new Employee1("Harry", 50000);
         System.out.println(e.getName() + " " + e.getSalary());
     }
 }
