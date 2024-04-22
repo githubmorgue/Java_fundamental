d94eb67b58137a551dc0a0bdda49a8f05b2349b7
7zZFHpYyu9 <jianguoh003@gmail.com>
2024-04-22 19:49:26 +0800

    Queues

---
+++ Stacks-Queues-Deques/Queues/LinkedQueue.java
@@ -0,0 +1,10 @@
+public class LinkedQueue<E> implements Queue<E> {
+    private SinglyLinkedList<E> list = new SinglyLinkedList<>();
+    public LinkedQueue() { }
+    public int size() { return list.size(); }
+    public boolean inEmpty() { return list.isEmpty(); }
+    public void enqueue(E element) { list.addLast(element); }
+    public E first() { return list.first(); }
+    public E dequeue() { return list.removeFirst(); }
+} 
+
