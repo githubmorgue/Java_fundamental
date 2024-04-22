b868b21d82750228f1a8b6761349d5bf7a41aac6
7zZFHpYyu9 <jianguoh003@gmail.com>
2024-04-22 18:45:37 +0800

    ArrayStack

--- ArrayStack.java
+++ ArrayStack.java
@@ -1,16 +1,16 @@
 public class ArrayStack<E> implements Stack<E> {
-    public static final int CAPACITY = 1000;
-    private E[] data;
-    private int t = -1;
-    public ArrayStack() { this(CAPACITY); }
-    public ArrayStack(int capacity) {
-        data  =(E[]) new Object[capacity];
+    public static final int CAPACITY = 1000;    // default array capacity
+    private E[] data;                           // generic array used for storage
+    private int t = -1;                         // index of the top element in stack
+    public ArrayStack() { this(CAPACITY); }     // constructs stack with default capacity
+    public ArrayStack(int capacity) {           // constructs stack with given capacity
+        data  =(E[]) new Object[capacity];      // safe cast; complier may given warning
     }
     public int size() { return (t+1); }
     public boolean isEmpty() { return (t == -1); }
     public void push(E e) throws IllegalStateException {
         if (size() == data.length) throw new IllegalStateException("Stack is full");
-        data[++t] = e;
+        data[++t] = e;                          // increment t before storing new item
     }
     public E top() {
         if (isEmpty()) return null;
@@ -19,7 +19,7 @@ public class ArrayStack<E> implements Stack<E> {
     public E pop() {
         if (isEmpty()) return null;
         E answer = data[t];
-        data[t] = null;
+        data[t] = null;                         // dereference to help garbage collection
         t--;
         return answer;
     }
